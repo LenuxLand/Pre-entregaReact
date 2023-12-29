80cab4dd562deb5fce82427bfa02d1337587b797
Lenux <lenux2112@gmail.com>
2023-12-29 08:20:12 -0300

    se agrego firebase

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuIvGhXlMAfSxfqsLPH3OniAJUMUUeNHI",
+  authDomain: "coderhouse-30b8b.firebaseapp.com",
+  projectId: "coderhouse-30b8b",
+  storageBucket: "coderhouse-30b8b.appspot.com",
+  messagingSenderId: "1039410377687",
+  appId: "1:1039410377687:web:8abfac37b73503a5409192"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
